873f4a73dd1c8cd38042af21cc894db74181dd25
nakao107107 <imakeanewstory@gmail.com>
2020-06-09 17:30:48 +0900

    imageをstaticから呼び出すページを追加

---
+++ pages/static.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <button class="btn cu-btn btn-primary">aa</button>
+    <img 
+    src="images/pikachu.png"
+    alt="picachu">
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
