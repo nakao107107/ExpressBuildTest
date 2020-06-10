4afd68a05628edc8c8ca0695c2dd5d558f9e208b
nakao107107 <imakeanewstory@gmail.com>
2020-06-10 09:52:39 +0900

    index.js再度修正

--- server/handler.js
+++ server/handler.js
@@ -1,28 +1,8 @@
-'use strict';
-const awsServerlessExpress = require('aws-serverless-express');
-const app = require('./index');
+const awsServerlessExpress = require('aws-serverless-express')
+const app = require('./index')
 
-const binaryMimeTypes = [
-  'application/javascript',
-  'application/json',
-  'application/octet-stream',
-  'application/xml',
-  'font/eot',
-  'font/opentype',
-  'font/otf',
-  'image/jpeg',
-  'image/png',
-  'image/svg+xml',
-  'image/x-icon',
-  'text/comma-separated-values',
-  'text/css',
-  'text/html',
-  'text/javascript',
-  'text/plain',
-  'text/text',
-  'text/xml'
-];
-const server = awsServerlessExpress.createServer(app, null, binaryMimeTypes);
+const server = awsServerlessExpress.createServer(app)
 
-module.exports.handler = (event, context) =>
-  awsServerlessExpress.proxy(server, event, context);
+module.exports.render = (event, context) => {
+  awsServerlessExpress.proxy(server, event, context)
+}
--- server/index.js
+++ server/index.js
@@ -1,16 +1,17 @@
 const express = require('express')
-const { Nuxt } = require('nuxt')
+const {Nuxt} = require('nuxt')
+const awsServerlessExpressMiddleware = require('aws-serverless-express/middleware')
+
 const app = express()
 
+app.use(awsServerlessExpressMiddleware.eventContext())
+
 let config = require('../nuxt.config.js')
+const nuxt = new Nuxt(config)
 
-async function start() {
-  // Init Nuxt.js
-  const nuxt = new Nuxt(config)
+app.use((req, res) => {
+  req.url = `${config.router.base}${req.url}`.replace('//', '/')
+  nuxt.render(req, res)
+})
 
-  await nuxt.ready()
-
-  // Give nuxt middleware to express
-  app.use(nuxt.render)
-}
-start()
+module.exports = app
